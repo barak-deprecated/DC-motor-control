68f2d23f3173327892dae78c7d452a7a0e1508ed
Barak Alon <barak.s.alon@gmail.com>
2014-09-28 21:17:38 -0700

    Added motor driving logic

--- DC_motor_control/DC_motor_control.ino
+++ DC_motor_control/DC_motor_control.ino
@@ -2,12 +2,6 @@ int potPin = 0;
 int posMotorPin = 3;
 int negMotorPin = 5;
 
-void drive_motor(int reading){
-  // Do the logic conversion of the potentiometer reading into output signals
-  // that the motor circuit can use.
-  
-}
-
 void setup(){
   // Set the pin mode for the PWM output pins to drive the motor  
   pinMode(posMotorPin, OUTPUT);
@@ -23,14 +17,29 @@ void loop(){
   // Read the analog voltage from the potentiometer
   // (This will do an analog-to-digital conversion (ADC) with 10 bit
   // resolution, meaning the it stores a reads a voltage between
-  // 0-5 V as an integer between 0 and 1024)  
+  // 0-5 V as an integer between 0 and 1023)  
   int potReading = analogRead(potPin);
   
-  // Send that potentiometer reading to a function that will handle driving 
-  // the motor
-  drive_motor(potReading);
+  // Convert the 0-1023 potentiometer reading to a value between -255 and 255
+  // (255 is the Arduino max PWM duty cycle. Positive values will drive the
+  // motor in one direction, negative values will drive it the other)
+  // (Consequently, 0 motor speed will be directly in between 0-1023)
+  outputSignal = map(potreading, 0, 1023, -255, 255);
   
-  // Print the potentiometer reading to the serial monitor so we can see
-  // what's going on
-  Serial.println(potReading);
+  // Print the readings to the serial monitor so we can see what's going on
+  Serial.print("Pot reading: ");
+  Serial.print(potReading);
+  Serial.print("\tOutput signal: ");
+  Serial.println(outputSignal);
+  
+  // If the output signal is a positive value, send the signal through
+  // the output pin that handles positive values
+  if(correctedReading >= 0){
+    analogWrite(posMotorPin, outputSignal);
+  }
+  // If the output signal is a negative value, send the singal through
+  // the output pin that handles negative values
+  else{
+    analogWrite(negMotorPin, outputSignal);
+  }  
 }
